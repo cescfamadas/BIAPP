0f2c308c5d44a14da00862ee9a0e7dd74a5aa942
francesc famadas <kiski97@gmail.com>
2020-06-01 13:05:59 +0200

    prod debug

--- app.py
+++ app.py
@@ -48,16 +48,16 @@ def index():
     return render_template('index.html')
 
 #region<upload>
-@app.route('/upload')  
-def upload():  
-    return render_template("file_upload_form.html")  
+# @app.route('/upload')  
+# def upload():  
+#     return render_template("file_upload_form.html")  
  #endregion
 
 if __name__ == "__main__":
     generateDF()
     mode = os.getenv("MODE")
     if mode == "prod":
-        app.run()
+        app.run(debug=True)
     else:
         app.run(debug=True)
     # https://sarahleejane.github.io/learning/python/2015/08/09/simple-tables-in-webapps-using-flask-and-pandas-with-python.html
